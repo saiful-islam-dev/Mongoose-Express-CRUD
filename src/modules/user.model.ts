49ccf4e1ef653a7e9cfe4f5c2d63cf77c5090a8d
Saiful264 <saifulislamx264@gmail.com>
2023-11-25 12:16:35 +0600

    interface and model  done declared

---
+++ src/modules/user.model.ts
@@ -0,0 +1,86 @@
+import { Schema } from 'mongoose';
+import { TAddress, TFullName, TOrrder, TUser } from './users/user.interface';
+
+const fullNameSchema = new Schema<TFullName>({
+  firstName: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+});
+
+const addressSchema = new Schema<TAddress>({
+  street: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+});
+
+const ordersSchema = new Schema<TOrrder>({
+  productName: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
+
+const userSchema = new Schema<TUser>({
+  userId: {
+    type: Number,
+    required: [true, 'Student ID is required'],
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  fullName: {
+    firstName: fullNameSchema,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  isActive: {
+    type: Boolean,
+    required: true,
+  },
+  hobbies: {
+    type: [{ type: String }],
+  },
+  address: {
+    type: addressSchema,
+    required: true,
+  },
+  orders: [ordersSchema],
+});
